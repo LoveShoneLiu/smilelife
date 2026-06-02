e255042c85335d26e18c32910ca0a22154275086
shaofeiliu <2397667718@qq.com>
2026-06-03 10:23:36 +1200

    feat: add live update

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,14 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'SmileLife',
-  webDir: 'www'
+  webDir: 'www',
+  plugins: {
+    LiveUpdates: {
+      appId: '3299359a',
+      channel: 'Production',
+      autoUpdateMethod: 'background'
+    }
+  }
 };
 
 export default config;
